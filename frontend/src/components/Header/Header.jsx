2236df0fcece42d6da4426e0bb15e291ee1e6b63
Fred <druetfrederique27@gmail.com>
2024-09-23 20:49:26 +0200

    edit profil

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,8 @@ function Header() {
   return (
     <div className="header">
       <h1>
-        "Bonjour, je m'appelle Frédérique Druet, <br /> je suis aspirante Analyste en Cybersécurité"
+        "Bonjour, je m'appelle Frédérique Druet, <br /> je suis en formation de
+        Technicien Supérieur Systèmes et Réseaux à la Wild Code School"
       </h1>
       <CVButton />
     </div>
